217c62bc5aa0dcca7d56523dbf692451c9aa0095
vakho1 <vakhtangi.batsikadze.1@btu.edu.ge>
2021-06-27 11:13:40 +0400

    Eager load and author's route

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Models\Post;
 use App\Models\Category;
+use App\Models\User;
 use Spatie\YamlFrontMatter\YamlFrontMatter;
 use Illuminate\Support\Facades\DB;
 
@@ -23,7 +24,7 @@ Route::get('/', function () {
     //         logger($query->sql, $query->bindings);
     //     });
      return view('posts', [
-         "posts"=>Post::with("category")->get()
+         "posts"=>Post::latest()->get()
      ]);
 });
 Route::get('/posts/{post:slug}', function (Post $post) {
@@ -35,9 +36,17 @@ Route::get('/posts/{post:slug}', function (Post $post) {
     ]);
 });
 
-Route::get("/categories/{category:slug}", function(Category $category){
+Route::get("/categories/{category:slug}", function(
+    Category $category){
     return view('posts', [
         "posts"=>$category->posts
+    ]);
+
+});
+
+Route::get("/author/{author:username}", function(User $author){
+    return view('posts', [
+        'posts' => $author->posts
     ]);
 
 });
